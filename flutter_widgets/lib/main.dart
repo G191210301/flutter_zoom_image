bf013dde2281013a052d18690079e04c136905da
huseyin <huseyinyeni94@gmail.com>
2022-12-07 15:53:03 +0300

    zoom image

---
+++ flutter_widgets/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Zoom Image'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late TransformationController controller;
+  TapDownDetails? tabDownDetails;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TransformationController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(24),
+          child: buildImage(),
+        ),
+      ),
+    );
+  }
+
+  Widget buildImage() => GestureDetector(
+        onDoubleTapDown: (details) => tabDownDetails = details,
+        onDoubleTap: () {
+          final position = tabDownDetails!.localPosition;
+
+          final double scale = 3;
+          final x = -position.dx * (scale - 1);
+          final y = -position.dy * (scale - 1);
+
+          final zoomed = Matrix4.identity()
+            ..translate(x, y)
+            ..scale(scale);
+          final value = controller.value.isIdentity() ? zoomed : Matrix4.identity();
+          controller.value = value;
+        },
+        child: InteractiveViewer(
+            clipBehavior: Clip.none,
+            transformationController: controller,
+            panEnabled: false,
+            scaleEnabled: false,
+            child: AspectRatio(aspectRatio: 1, child: Image.asset('images/catimage.jpg'))),
+      );
+}
